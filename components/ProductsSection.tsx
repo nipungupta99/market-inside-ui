deaf947d039593516876c023fe26230ca2fc437e
nipungupta99 <gupta.nipun99@gmail.com>
2024-05-17 14:15:24 +0530

    Added UI for product section

--- components/ProductsSection.tsx
+++ components/ProductsSection.tsx
@@ -1,13 +1,43 @@
 import Image from "next/image";
 import Button from "./Button";
+import { Accordion, AccordionItem } from "@szhsin/react-accordion";
+
+const ParentAccordionHeader = ({ title }: { title: string }) => {
+  return (
+    <div className="accordion__parent-header">
+      <h2 className="accordion__parent-header-title">{title}</h2>
+    </div>
+  );
+};
+
+const ChildAccordionHeader = ({ title }: { title: string }) => {
+  return <div className="accordion__child-header">{title}</div>;
+};
+
+const ChildAccordionContent = ({
+  imgSrc,
+  description,
+}: {
+  imgSrc: string;
+  description: string;
+}) => {
+  return (
+    <div className="accordion__child-content">
+      <div className="accordion__child-content-image">
+        <Image src={imgSrc} alt="product" width="300" height="300" />
+      </div>
+      <p className="accordion__child-content-description">{description}</p>
+    </div>
+  );
+};
 
 const ProductsSection = () => {
   return (
-    <section className="products">
-      <div className="container mx-auto my-16">
-        <div className="">
-          <h2 className="text-[40px] font-semibold">OUR PRODUCTS</h2>
-          <p className="text-[#6D6D6D] my-3 mb-8 leading-[1.8em]">
+    <section className="products-section">
+      <div className="products-section__container">
+        <div className="products-section__header">
+          <h2 className="products-section__title">OUR PRODUCTS</h2>
+          <p className="products-section__description">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -16,43 +46,37 @@ const ProductsSection = () => {
             pariatur
           </p>
         </div>
-        <div className="flex gap-8">
-          <div className="border border-gray-300 p-10 px-8 rounded-2xl flex flex-col gap-5 min-w-[250px]">
-            <div className=" text-[14px] font-medium border-b-4 border-b-[var(--color-highlight)] text-[var(--color-highlight)] px-3 py-2">
+        <div className="products-section__content md:flex">
+          <div className="products-section__menu">
+            <div className="products-section__menu-item products-section__menu-item--active">
               PLATFORM
             </div>
-            <div className="text-[#747474] text-[14px] font-medium px-3 py-2">
-              API
-            </div>
-            <div className="text-[#747474] text-[14px] font-medium px-3 py-2">
-              DATA
-            </div>
+            <div className="products-section__menu-item">API</div>
+            <div className="products-section__menu-item">DATA</div>
           </div>
-          <div className="border border-gray-300 p-10 px-8 rounded-2xl flex flex-col gap-5">
-            <h3 className="text-center text-[25px]">
+          <div className="products-section__details">
+            <h3 className="products-section__details-title">
               Technological advancements provide a competitive edge
             </h3>
-            <div className="flex gap-8 mt-10">
-              <div>
-                <div className="flex gap-4 my-5">
-                  <Button className=" border border-[var(--color-highlight)] text-black rounded-xl py-3 h-[45px]">
-                    VISUALISE VIEW
-                  </Button>
-                  <Button className="  text-black rounded-xl py-3 h-[45px]">
-                    VIEW PROFILE{" "}
-                  </Button>
-                  <Button className="  text-black rounded-xl py-3 h-[45px]">
-                    QUICK VIEW
-                  </Button>
-                </div>
-                <p className="leading-[1.8em]">
-                  Gather information on international trade in a visually
-                  appealing way. Learn about the market trends easily and get
-                  all the essential information on the import and export of a
-                  country in the form of interactive graphics
-                </p>
+            <div className="products-section__details-content">
+              <div className="products-section__details-buttons">
+                <Button className="products-section__details-button">
+                  VISUALISE VIEW
+                </Button>
+                <Button className="products-section__details-button">
+                  VIEW PROFILE
+                </Button>
+                <Button className="products-section__details-button">
+                  QUICK VIEW
+                </Button>
               </div>
-              <div className="min-w-[300px]">
+              <p className="products-section__details-description">
+                Gather information on international trade in a visually
+                appealing way. Learn about the market trends easily and get all
+                the essential information on the import and export of a country
+                in the form of interactive graphics
+              </p>
+              <div className="products-section__details-image">
                 <Image
                   src="/assets/product-image-1.webp"
                   alt="product"
@@ -63,6 +87,137 @@ const ProductsSection = () => {
             </div>
           </div>
         </div>
+        <Accordion className="products-section__accordion md:hidden">
+          <AccordionItem
+            header={<ParentAccordionHeader title="PLATFORM" />}
+            className="products-section__accordion-item"
+          >
+            <h3 className="products-section__details-title mt-5">
+              Technological advancements provide a competitive edge
+            </h3>
+            <Accordion>
+              <AccordionItem
+                header={<ChildAccordionHeader title="VISUALISE VIEW" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+              <AccordionItem
+                header={<ChildAccordionHeader title="VIEW PROFILE" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+              <AccordionItem
+                header={<ChildAccordionHeader title="QUICK VIEW" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+            </Accordion>
+          </AccordionItem>
+          <AccordionItem
+            header={<ParentAccordionHeader title="API" />}
+            className="products-section__accordion-item"
+          >
+            <h3 className="products-section__details-title mt-5">
+              Technological advancements provide a competitive edge
+            </h3>
+            <Accordion>
+              <AccordionItem
+                header={<ChildAccordionHeader title="VISUALISE VIEW" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+              <AccordionItem
+                header={<ChildAccordionHeader title="VIEW PROFILE" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+              <AccordionItem
+                header={<ChildAccordionHeader title="QUICK VIEW" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+            </Accordion>
+          </AccordionItem>
+          <AccordionItem
+            header={<ParentAccordionHeader title="DATA" />}
+            className="products-section__accordion-item"
+          >
+            <h3 className="products-section__details-title mt-5">
+              Technological advancements provide a competitive edge
+            </h3>
+            <Accordion>
+              <AccordionItem
+                header={<ChildAccordionHeader title="VISUALISE VIEW" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+              <AccordionItem
+                header={<ChildAccordionHeader title="VIEW PROFILE" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+              <AccordionItem
+                header={<ChildAccordionHeader title="QUICK VIEW" />}
+              >
+                <ChildAccordionContent
+                  imgSrc="/assets/product-image-1.webp"
+                  description="Gather information on international trade in a visually
+                  appealing way. Learn about the market trends easily and get
+                  all the essential information on the import and export of a
+                  country in the form of interactive graphics"
+                />
+              </AccordionItem>
+            </Accordion>
+          </AccordionItem>
+        </Accordion>
       </div>
     </section>
   );
